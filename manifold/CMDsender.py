52533186fc162216ff995f56e4c275c0ddd1e98f
lightcone <zyhbum@foxmail.com>
2020-12-08 15:16:44 +0800

    gyt
    gyt

--- manifold/CMDsender.py
+++ manifold/CMDsender.py
@@ -15,6 +15,14 @@ class CMDSend():
         """
         print(s)
         # res = self.ser.write(s)
-        res = self.ser.write(s.encode("UTF-8"))
+        # res = self.ser.write(s.encode("UTF-8"))
+        res = self.ser.write("AAFFE10200008C81".encode("UTF-8"))
         print("写总字节数:{}".format(res))
         self.ser.close()#关闭串口
+
+    def reception(self):#接收函数
+        while True:
+            while self.ser.inWaiting()>0:
+                myout = self.ser.read(7)#读取串口传过来的字节流，这里我根据文档只接收7个字节的数据
+                my_need = int(hex(int(myout,16)),16)#将十六进制转化为十进制
+                print(my_need)
--- manifold/application.py
+++ manifold/application.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     cmd_sender = CMDSend()
     # 获取飞控数据
     # communicater = Communication_LX(0xff, 0xe1, ["0000"])
-    communicater = Communication_LX(0xff, 0xe0, [10, 00, "01"])
-
+    communicater = Communication_LX(0xff, 0xe1, ["0000"])
+    import sys
+    sys.stdout.write(communicater.getInfo())
     cmd_sender.send_LX(communicater.getInfo())
+    cmd_sender.reception()
